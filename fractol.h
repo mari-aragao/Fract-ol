318a8205a20e2dcf45356e67ec0a402956c3a4cd
maragao <maragao@student.42.rio>
2022-10-01 19:38:08 -0300

    add

---
+++ fractol.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maragao <maragao@student.42.rio>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/01 16:18:04 by maragao           #+#    #+#             */
+/*   Updated: 2022/10/01 16:57:46 by maragao          ###   ########.rio      */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_H
+# define FRACTOL_H
+# include "mlx/mlx.h"
+# define SIZE_WIN	600	
+
+typedef struct	s_complex
+{
+	double	re;
+	double	im;
+}				t_complex;
+
+typedef struct	s_data
+{
+	void	*img;
+	char	*addr;
+	int		bpp; //bits per pixel
+	int		line_lenght;
+	int		endian;
+}				t_data;
+
+#endif
