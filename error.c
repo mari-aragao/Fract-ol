64b072bc6bff9942a39e2bc88aacc1db848dd4f0
maragao <maragao@student.42.rio>
2022-11-24 21:18:35 -0800

    update

--- error.c
+++ error.c
@@ -6,7 +6,7 @@
 /*   By: maragao <maragao@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 15:15:04 by maragao           #+#    #+#             */
-/*   Updated: 2022/11/23 20:12:02 by maragao          ###   ########.rio      */
+/*   Updated: 2022/11/24 21:16:04 by maragao          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "fractol.h"
@@ -17,5 +17,8 @@ int	error_msg(void)
 	write(1, "julia 0.285 0.01\n", 17);
 	write(1, "julia -0.8 0.156\n", 17);
 	write(1, "julia -0.835 -0.2321\n", 21);
+	write(1, "julia -1.037 0.17\n", 18);
+	write(1, "julia -0.52 0.57\n", 17);
+	write(1, "julia 0.285 0.50\n", 17);
 	return (-5);
 }
--- error_bonus.c
+++ error_bonus.c
@@ -6,18 +6,21 @@
 /*   By: maragao <maragao@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 15:15:04 by maragao           #+#    #+#             */
-/*   Updated: 2022/11/24 19:50:53 by maragao          ###   ########.fr       */
+/*   Updated: 2022/11/24 21:16:37 by maragao          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "fractol.h"
 
 int	error_msg(void)
 {
-	write(1, "Example of valid argument:\n", 27);
+	write(1, "Examples of valid arguments:\n", 29);
 	write(1, "mandelbrot\n", 11);
 	write(1, "julia 0.285 0.01\n", 17);
 	write(1, "julia -0.8 0.156\n", 17);
 	write(1, "julia -0.835 -0.2321\n", 21);
+	write(1, "julia -1.037 0.17\n", 18);
+	write(1, "julia -0.52 0.57\n", 17);
+	write(1, "julia 0.285 0.50\n", 17);
 	write(1, "\"burning ship\"\n", 15);
 	return (-5);
 }
